c227c491ba83b796fe0caa805d7f7a7ad0e50f68
Lukas May <76849604+lukasmay@users.noreply.github.com>
2023-02-03 15:59:04 -0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,24 +1,24 @@
-@rem
+@rem Test
 @rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem Test
+@rem Licensed under the Apache License, Version 2.0 the "License"
 @rem you may not use this file except in compliance with the License.
 @rem You may obtain a copy of the License at
-@rem
+@rem Test
 @rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
+@rem Test
 @rem Unless required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
-@rem
+@rem Test
 
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
-@rem
+@rem Test
 @rem  Gradle startup script for Windows
-@rem
+@rem Test
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
