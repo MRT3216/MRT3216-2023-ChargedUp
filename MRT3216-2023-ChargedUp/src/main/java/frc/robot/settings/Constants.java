a16ac848e252a426092675154fe29083f4121d74
Jacob Shepherd <jacobshepherd1005@gmail.com>
2023-02-14 12:53:45 -0700

    Update Constants.java

--- MRT3216-2023-ChargedUp/src/main/java/frc/robot/settings/Constants.java
+++ MRT3216-2023-ChargedUp/src/main/java/frc/robot/settings/Constants.java
@@ -14,10 +14,10 @@ import edu.wpi.first.wpilibj.Filesystem;
 public final class Constants {
     public static final class Drivetrain {
         // TODO: set these values
-        public static final double LEFT_FRONT_STEER_OFFSET = -Math.toRadians(8.43); //rotating inverse of other wheels
-        public static final double RIGHT_FRONT_STEER_OFFSET = -Math.toRadians(76.99 + 180);
-        public static final double LEFT_REAR_STEER_OFFSET = -Math.toRadians(24.52);
-        public static final double RIGHT_REAR_STEER_OFFSET = -Math.toRadians(-67.15 + 180);
+        public static final double LEFT_FRONT_STEER_OFFSET = -Math.toRadians(-0.35); //rotating inverse of other wheels
+        public static final double RIGHT_FRONT_STEER_OFFSET = -Math.toRadians(77.87);
+        public static final double LEFT_REAR_STEER_OFFSET = -Math.toRadians(-0.26);
+        public static final double RIGHT_REAR_STEER_OFFSET = -Math.toRadians(-68.20);
 
         // TODO: set these values
         public static final double WHEELBASE_METERS = 0.5461;
