efe67790982da63086f2b6f3b6c730705034592c
Daniel Lande <76849604+lukasmay@users.noreply.github.com>
2023-02-18 12:06:08 -0700

    Added Code Formatter
    Ran the code formatter once.

--- MRT3216-2023-ChargedUp/src/main/java/frc/robot/AutoChooser.java
+++ MRT3216-2023-ChargedUp/src/main/java/frc/robot/AutoChooser.java
@@ -4,13 +4,6 @@
 
 package frc.robot;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.function.Supplier;
-
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -20,53 +13,58 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.settings.Constants.Directories;
 import frc.robot.subsystems.SwerveSubsystem;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.function.Supplier;
 
 /** Add your docs here. */
 public class AutoChooser {
 
-    private static AutoChooser instance;
-    private Dictionary<String, Trajectory> trajectories;
-    private SendableChooser<Supplier<Command>> chooser;
-    private SwerveSubsystem swerveSystem;
+  private static AutoChooser instance;
+  private Dictionary<String, Trajectory> trajectories;
+  private SendableChooser<Supplier<Command>> chooser;
+  private SwerveSubsystem swerveSystem;
 
-    private AutoChooser() {
-        chooser = new SendableChooser<>();
-        chooser.setDefaultOption("Do Nothing", () -> new WaitCommand(0));
-        this.swerveSystem = SwerveSubsystem.getInstance();
+  private AutoChooser() {
+    chooser = new SendableChooser<>();
+    chooser.setDefaultOption("Do Nothing", () -> new WaitCommand(0));
+    this.swerveSystem = SwerveSubsystem.getInstance();
+  }
+
+  public static AutoChooser getInstance() {
+    if (instance == null) {
+      instance = new AutoChooser();
     }
+    return instance;
+  }
 
-    public static AutoChooser getInstance() {
-        if (instance == null) {
-            instance = new AutoChooser();
+  public void readTrajectories() {
+    File pathsDirectory = new File(Directories.pathsDirectory);
+    File[] files = pathsDirectory.listFiles();
+    this.trajectories = new Hashtable<String, Trajectory>();
+    for (int i = 0; i < files.length; i++) {
+      try {
+        Path trajectoryPath = files[i].toPath();
+        System.out.println("PATH " + i + ": " + trajectoryPath.toString());
+        if (files[i].isFile()) {
+          trajectories.put(files[i].getName(), TrajectoryUtil.fromPathweaverJson(trajectoryPath));
         }
-        return instance;
+      } catch (IOException ex) {
+        DriverStation.reportError(
+            "Unable to open trajectory: " + files[i].toString(), ex.getStackTrace());
+      }
     }
+  }
 
-    public void readTrajectories() {
-        File pathsDirectory = new File(Directories.pathsDirectory);
-        File[] files = pathsDirectory.listFiles();
-        this.trajectories = new Hashtable<String, Trajectory>();
-        for (int i = 0; i < files.length; i++) {
-            try {
-                Path trajectoryPath = files[i].toPath();
-                System.out.println("PATH " + i + ": " + trajectoryPath.toString());
-                if (files[i].isFile()) {
-                    trajectories.put(files[i].getName(),
-                            TrajectoryUtil.fromPathweaverJson(trajectoryPath));
-                }
-            } catch (IOException ex) {
-                DriverStation.reportError("Unable to open trajectory: " + files[i].toString(),
-                        ex.getStackTrace());
-            }
-        }
-    }
+  public void populateAutoChooser() {
 
-    public void populateAutoChooser() {
+    SmartDashboard.putData(chooser);
+  }
 
-        SmartDashboard.putData(chooser);
-    }
-
-    public Command getAutoCommand() {
-        return chooser.getSelected().get();
-    }
+  public Command getAutoCommand() {
+    return chooser.getSelected().get();
+  }
 }
--- MRT3216-2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
+++ MRT3216-2023-ChargedUp/src/main/java/frc/robot/RobotContainer.java
@@ -2,12 +2,11 @@ package frc.robot;
 
 // region Imports
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.OI.OIUtils;
 import frc.robot.commands.TeleDrive;
@@ -22,118 +21,150 @@ import io.github.oblarg.oblog.annotations.Log;
 // endregion
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // #region Fields
+  // #region Fields
 
-    private static RobotContainer instance;
-    @Log.BooleanBox(name = "Gyro Con.", methodName = "gyroConnected", rowIndex = 1, columnIndex = 3, width = 1, height = 1)
-    private SwerveSubsystem driveSystem;
+  private static RobotContainer instance;
 
-    private AutoChooser autoChooser;
-    private double autoStartDelayTime;
-    private double translationExpo;
-    private double rotationExpo;
-    private CommandXboxController controller;
+  @Log.BooleanBox(
+      name = "Gyro Con.",
+      methodName = "gyroConnected",
+      rowIndex = 1,
+      columnIndex = 3,
+      width = 1,
+      height = 1)
+  private SwerveSubsystem driveSystem;
 
-    // #endregion
+  private AutoChooser autoChooser;
+  private double autoStartDelayTime;
+  private double translationExpo;
+  private double rotationExpo;
+  private CommandXboxController controller;
 
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    private RobotContainer() {
-        this.controller = new CommandXboxController(0);
-        this.autoStartDelayTime = Constants.Auto.kStartDelayTime;
-        this.translationExpo = Constants.OI.kTranslationExpo;
-        this.rotationExpo = Constants.OI.kRotationnExpo;
+  // #endregion
 
-        this.initSubsystems();
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private RobotContainer() {
+    this.controller = new CommandXboxController(0);
+    this.autoStartDelayTime = Constants.Auto.kStartDelayTime;
+    this.translationExpo = Constants.OI.kTranslationExpo;
+    this.rotationExpo = Constants.OI.kRotationnExpo;
 
-        // The first argument is the root container
-        // The second argument is whether logging and config should be given separate
-        // tabs
-        Logger.configureLoggingAndConfig(this, false);
-        // Logger.configureLogging(this);
+    this.initSubsystems();
 
-        // Configure the button bindings
-        configureButtonBindings();
+    // The first argument is the root container
+    // The second argument is whether logging and config should be given separate
+    // tabs
+    Logger.configureLoggingAndConfig(this, false);
+    // Logger.configureLogging(this);
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  public void initSubsystems() {
+    this.driveSystem = SwerveSubsystem.getInstance();
+    this.autoChooser = AutoChooser.getInstance();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    if (driveSystem != null && controller != null) {
+      driveSystem.setDefaultCommand(
+          new TeleDrive(
+              driveSystem,
+              () ->
+                  OIUtils.modifyAxis(controller.getLeftY(), this.translationExpo)
+                      * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
+              () ->
+                  OIUtils.modifyAxis(controller.getLeftX(), this.translationExpo)
+                      * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
+              () ->
+                  OIUtils.modifyAxis(controller.getRightX(), this.rotationExpo)
+                      * Drivetrain.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
+              true));
     }
 
-    public void initSubsystems() {
-        this.driveSystem = SwerveSubsystem.getInstance();
-        this.autoChooser = AutoChooser.getInstance();
+    // Resets the Robots Odometry and Gyro values
+    controller.y().onTrue(runOnce(() -> this.driveSystem.resetGyroAndOdometry(true)));
+  }
+
+  public void disablePIDSubsystems() {}
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    return this.autoChooser.getAutoCommand();
+  }
+
+  public static RobotContainer getInstance() {
+    if (instance == null) {
+      // if instance is null, initialize
+      instance = new RobotContainer();
     }
+    return instance;
+  }
 
-    /**
-     * Use this method to define your button->command mappings. Buttons can be
-     * created by instantiating a {@link GenericHID} or one of its subclasses
-     * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
-     * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
-        if (driveSystem != null && controller != null) {
-            driveSystem.setDefaultCommand(new TeleDrive(
-                    driveSystem,
-                    () -> OIUtils.modifyAxis(controller.getLeftY(), this.translationExpo)
-                            * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
-                    () -> OIUtils.modifyAxis(controller.getLeftX(), this.translationExpo)
-                            * Drivetrain.MAX_VELOCITY_METERS_PER_SECOND,
-                    () -> OIUtils.modifyAxis(controller.getRightX(), this.rotationExpo)
-                            * Drivetrain.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND,
-                    true));
-        }
+  public SwerveSubsystem getDriveSystem() {
+    return driveSystem;
+  }
 
-        // Resets the Robots Odometry and Gyro values
-        controller.y().onTrue(runOnce(() -> this.driveSystem.resetGyroAndOdometry(true)));
-    }
+  @Config(
+      name = "Auto Delay",
+      tabName = "Tuning",
+      methodName = "setStartDelayTime",
+      defaultValueNumeric = Auto.kStartDelayTime,
+      methodTypes = {double.class},
+      rowIndex = 1,
+      columnIndex = 1)
+  public void setStartDelayTime(double startDelayTime) {
+    this.autoStartDelayTime = startDelayTime;
+  }
 
-    public void disablePIDSubsystems() {
+  public double getAutoStartDelayTime() {
+    return this.autoStartDelayTime;
+  }
 
-    }
+  @Config.NumberSlider(
+      name = "Trans. Expo",
+      tabName = "Tuning",
+      defaultValue = Constants.OI.kTranslationExpo,
+      min = 0,
+      max = 100,
+      blockIncrement = 1,
+      rowIndex = 2,
+      columnIndex = 0,
+      height = 1,
+      width = 1)
+  public void setTranslationExpo(double expo) {
+    this.translationExpo = expo;
+  }
 
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        return this.autoChooser.getAutoCommand();
-    }
-
-    public static RobotContainer getInstance() {
-        if (instance == null) {
-            // if instance is null, initialize
-            instance = new RobotContainer();
-        }
-        return instance;
-    }
-
-    public SwerveSubsystem getDriveSystem() {
-        return driveSystem;
-    }
-
-    @Config(name = "Auto Delay", tabName = "Tuning", methodName = "setStartDelayTime", defaultValueNumeric = Auto.kStartDelayTime, methodTypes = {
-            double.class }, rowIndex = 1, columnIndex = 1)
-    public void setStartDelayTime(double startDelayTime) {
-        this.autoStartDelayTime = startDelayTime;
-    }
-
-    public double getAutoStartDelayTime() {
-        return this.autoStartDelayTime;
-    }
-
-    @Config.NumberSlider(name = "Trans. Expo", tabName = "Tuning", defaultValue = Constants.OI.kTranslationExpo, min = 0, max = 100, blockIncrement = 1, rowIndex = 2, columnIndex = 0, height = 1, width = 1)
-    public void setTranslationExpo(double expo) {
-        this.translationExpo = expo;
-    }
-
-    @Config.NumberSlider(name = "Rotation Expo", tabName = "Tuning", defaultValue = Constants.OI.kRotationnExpo, min = 0, max = 100, blockIncrement = 1, rowIndex = 2, columnIndex = 1, height = 1, width = 1)
-    public void setRotationExpo(double expo) {
-        this.rotationExpo = expo;
-    }
+  @Config.NumberSlider(
+      name = "Rotation Expo",
+      tabName = "Tuning",
+      defaultValue = Constants.OI.kRotationnExpo,
+      min = 0,
+      max = 100,
+      blockIncrement = 1,
+      rowIndex = 2,
+      columnIndex = 1,
+      height = 1,
+      width = 1)
+  public void setRotationExpo(double expo) {
+    this.rotationExpo = expo;
+  }
 }
